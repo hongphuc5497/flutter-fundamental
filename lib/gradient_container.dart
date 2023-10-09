cd502d811da1ed56782ce1f37ec342020cab8e95
Hong Phuc <hongphuc.dthp@gmail.com>
2023-10-10 02:06:23 +0700

    flutter: add images & btn handling - wip

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_fundamental/styled_text.dart';
@@ -13,13 +11,13 @@ import 'package:flutter_fundamental/styled_text.dart';
 Alignment startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-// 1st approach
+// * 1st approach
 // class GradientContainer extends StatelessWidget {
-  // GradientContainer({key}): super(key: key);
-  // Shorthand for the above line
+// GradientContainer({key}): super(key: key);
+// Shorthand for the above line
 
-  // postional agrs: const GradientContainer(this.colors, this.text, {super.key});
-  // named agrs:
+// postional agrs: const GradientContainer(this.colors, this.text, {super.key});
+// named agrs:
 //   const GradientContainer({super.key, required this.colors, required this.text});
 
 //   final List<Color> colors;
@@ -40,7 +38,7 @@ const endAlignment = Alignment.bottomRight;
 //   }
 // }
 
-// 2nd approach
+// * 2nd approach
 class GradientContainer extends StatelessWidget {
   // GradientContainer({key}): super(key: key);
   // Shorthand for the above line
@@ -49,10 +47,19 @@ class GradientContainer extends StatelessWidget {
   // named agrs:
   const GradientContainer(this.color1, this.color2, this.text, {super.key});
 
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.purple,
+        color2 = Colors.indigo,
+        text = 'Called from purple func';
+
   final Color color1;
   final Color color2;
 
   final String text;
+
+  void rollDice() {
+    print('Rolling dice...');
+  }
 
   @override
   Widget build(context) {
@@ -61,8 +68,14 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
-      child: Center(
-        child: StyledText(text),
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/dice-2.png',
+            width: 200,
+          ),
+          TextButton(onPressed: rollDice, child: StyledText(text)),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fundamental/gradient_container.dart';
 
 void main() {
-  const outputText = 'Lorem Ipsum';
+  const outputText = 'Roll Dice';
 
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
+      // body: GradientContainer.purple(),
       body: GradientContainer(
         Color.fromARGB(255, 33, 5, 109),
         Color.fromARGB(255, 68, 21, 149),
