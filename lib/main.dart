81a1e41212c01ac30d16f0137463d8b7f2359efd
Hong Phuc <hongphuc.dthp@gmail.com>
2023-10-09 16:08:54 +0700

    flutter: build custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.amber, Colors.grey],
-            begin: Alignment.topRight,
-            end: Alignment.bottomRight
-          ),
-        ),
-        child: const Center(
-          child: Text('Lorem Ipsum',
-            
-            style: TextStyle(
-              fontSize: 28,
-              color: Colors.white
-            )
-
-            ,
-          ),
-        ),
-      ),
+      body: GradientContainer(),
     ),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.amber, Colors.grey],
+            begin: Alignment.topRight,
+            end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Lorem Ipsum',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+      ),
+    );
+  }
 }
